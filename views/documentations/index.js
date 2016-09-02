4580ffd7aec89c04bb8bd0d855759976c29864bf
Scott Whitman <scott.w.whitman@gmail.com>
2016-09-02 13:53:17 -0700

    added profile

--- views/documentations/index.js
+++ views/documentations/index.js
@@ -1,19 +1,3 @@
-module.exports = {
-  title: "Scott's greatest dives sites app",
-  message: "Welcome to my personal api on the greatest dive sites I've been to! Here's what you need to know!",
-  documentationUrl: "https://github.com/scottwwhitman/express-personal-api",
-  baseUrl: "https://fierce-savannah-91724.herokuapp.com/",
-  endpoints: [
-    {method: "GET", path: "/api", description: "Describes all available endpoints"},
-    {method: "GET", path: "/api/profile", description: "Data about me"},
-    {method: "GET", path: "/api/divesites", description: "Get all my best divesites"},
-    {method: "POST", path: "/api/divesites", description: "Create a new divesite"},
-    {method: "GET", path: "/api/divesites/:id", description: "Get a divesite"},
-    {method: "PUT", path: "/api/divesites/:id", description: "Update a divesite"},
-    {method: "DELETE", path: "/api/divesites/:id", description: "Remove a divesite"},
-    {method: "GET", path: "/api/divesites/:divesite_id/animals", description: "Get all animals seen at a divesite"},
-    {method: "POST", path: "/api/divesites/:divesite_id/animals", description: "Add an animal seen at a divesite"},
-    {method: "PUT", path: "/api/divesites/:divesite_id/animals/:animal_id", description: "Update an animal seen at a divesite"},
-    {method: "PUT", path: "/api/divesites/:divesite_id/animals/:animal_id", description: "Remove an animal from a divesite"}
-  ]
-}
+module.exports.Api_documentation = require("./api_documentation.js");
+
+module.exports.Profile = require("./profile.js");
---
+++ views/documentations/profile.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "Scott Whitman",
+  githubLink: "https://github.com/scottwwhitman",
+  githubProfileImage: " ",
+  personalSiteLink: "https://scottwwhitman.github.io/",
+  currentCity: "San Francisco, CA",
+  pets: "None"
+}
