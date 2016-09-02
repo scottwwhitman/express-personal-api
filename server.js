702cb7b1398dfbb2c6325dc6866c743d6badacd1
Scott Whitman <scott.w.whitman@gmail.com>
2016-09-02 14:04:47 -0700

    reverted to docs and profile on server

--- server.js
+++ server.js
@@ -44,15 +44,45 @@ app.get('/', function homepage(req, res) {
 
 app.get('/api', function api_index(req, res) {
   // TODO: Document all your api endpoints below
-  var documents = require('./views/documentations/Api_documentation');
-  res.json(documents);
+  // var documents = require('./views/documentations/Api_documentation');
+  res.json(apiDocumentation);
 });
 
 app.get('/api/profile', function api_index(req, res) {
   // TODO: Document all your api endpoints below
-  var documents = require('./views/documentations/Profile');
-  res.json(documents);
+  // var documents = require('./views/documentations/Profile');
+  res.json(profile);
 });
+
+var profile = {
+  name: "Scott Whitman",
+  githubLink: "https://github.com/scottwwhitman",
+  githubProfileImage: " ",
+  personalSiteLink: "https://scottwwhitman.github.io/",
+  currentCity: "San Francisco, CA",
+  pets: "None"
+}
+
+var apiDocumentation = {
+  title: "Scott's greatest dives sites app",
+  message: "Welcome to my personal api on the greatest dive sites I've been to! Here's what you need to know!",
+  documentationUrl: "https://github.com/scottwwhitman/express-personal-api",
+  baseUrl: "https://fierce-savannah-91724.herokuapp.com/",
+  endpoints: [
+    {method: "GET", path: "/api", description: "Describes all available endpoints"},
+    {method: "GET", path: "/api/profile", description: "Data about me"},
+    {method: "GET", path: "/api/divesites", description: "Get all my best divesites"},
+    {method: "POST", path: "/api/divesites", description: "Create a new divesite"},
+    {method: "GET", path: "/api/divesites/:id", description: "Get a divesite"},
+    {method: "PUT", path: "/api/divesites/:id", description: "Update a divesite"},
+    {method: "DELETE", path: "/api/divesites/:id", description: "Remove a divesite"},
+    {method: "GET", path: "/api/divesites/:divesite_id/animals", description: "Get all animals seen at a divesite"},
+    {method: "POST", path: "/api/divesites/:divesite_id/animals", description: "Add an animal seen at a divesite"},
+    {method: "PUT", path: "/api/divesites/:divesite_id/animals/:animal_id", description: "Update an animal seen at a divesite"},
+    {method: "PUT", path: "/api/divesites/:divesite_id/animals/:animal_id", description: "Remove an animal from a divesite"}
+  ]
+}
+
 
 
 /**********
